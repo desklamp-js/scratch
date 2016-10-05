768db733d36bbeb765795c4993fd48d6865e52cd
sneakykate <sneakyk8@gmail.com>
2016-10-05 14:05:36 -0700

    make sure test folder included in commit

---
+++ test/desklamp-unit-tests.js
@@ -0,0 +1,11 @@
+describe('getRoutes', function() {
+  it('should retrieve routes', function() {
+    //HOW DO I TEST A FUNCTION WITHOUT A RETURN? PAUSING TO LOOK AT ENZYME
+
+    // for (var i = 0; i < 10; i++) {
+    //   var smallArray = makeList(100);
+    //   var clone = smallArray.slice();
+    //   expect(insertionSort(clone)).to.eql(smallArray.sort(inc));
+    // }
+  });
+});
