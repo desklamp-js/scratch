a5f221f1786c62597afcb1c47a5bb00414b384b3
Michael Kulinski <me@michaelkulinski.com>
2016-10-07 10:15:22 -0700

    created a testApp to test the functionality of our current framework

---
+++ testApp/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = (...props) => {
+  console.log(props)
+  return (
+    <div>
+      <h1>This the home page</h1>
+      <h1>Welcome!</h1>
+    </div>
+  );
+};
+
+export default Home;
