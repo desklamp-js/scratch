49b7d5aa379ca955b2ea322c3786ebb26487f6cc
sneakykate <sneakyk8@gmail.com>
2016-10-25 19:22:20 -0700

    enzyme_file

--- test/enzyme-test-desklamp.js
+++ test/enzyme-test-desklamp.js
@@ -4,15 +4,13 @@ import { expect } from 'chai';
 import sinon from 'sinon';
 import jsdom from 'jsdom';
 
-import {Container} from './../deployment/desklamp.js'
+import {Link} from 'desklamp'
 // from './../src/index.js'; // relative path working, does not like document.getElementById in index
 
-describe('Container', () =>{
-  it('Container.routeLink updates view in state', () => {
-    const wrapper = shallow(<Container />);
-
+describe('Link', () =>{
+  it('Link renders a single anchor tag', () => {
+    const wrapper = shallow(<Link />);
     console.log(wrapper.props);
-    const testInnerHtmlMock = "Login";
     wrapper.setState({view: "Profile"});
 
     wrapper.instance().routeLink(testInnerHtmlMock);
